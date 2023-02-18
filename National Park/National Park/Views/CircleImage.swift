ab3b4bcc475cbf2e7bf8834efe95e33e14720316
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-02-18 16:57:28 +0530

    Update CircleImage.swift

--- National Park/National Park/Views/CircleImage.swift
+++ National Park/National Park/Views/CircleImage.swift
@@ -15,8 +15,10 @@ struct CircleImage: View {
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
+                    
             }
             .shadow(radius: 7)
+            
     }
 }
 
