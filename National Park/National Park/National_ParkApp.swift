cf717c7ec3227cdd3fa4e37c51258175759b8d4f
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-02-18 17:00:45 +0530

    Update National_ParkApp.swift

--- National Park/National Park/National_ParkApp.swift
+++ National Park/National Park/National_ParkApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct National_ParkApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
