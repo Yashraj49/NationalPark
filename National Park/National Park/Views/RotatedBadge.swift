0752a80cb977a5fd03542a0616e599c1d2f0eb13
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-02-19 22:51:37 +0530

    Add files via upload

---
+++ National Park/National Park/Views/RotatedBadge.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadge.swift
+//  National Park
+//
+//  Created by Yashraj jadhav on 19/02/23.
+//
+
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
